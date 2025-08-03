1d0bff8c254f03cdbb1f4d4fac30bcb56f0161b8
Nabeel Iqbal <nabeeliq@usc.edu>
2025-08-02 21:16:01 -0700

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>🚀 Welcome to My First React App!</h1>
+      <p>This is running on Vite + React.</p>
+      <button onClick={() => alert("Hello! You clicked the button.")}>
+        Click Me
+      </button>
+    </div>
+  );
+}
+
+export default App;
